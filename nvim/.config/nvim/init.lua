bda8019680fe58ec184d4f87ee944b8740acea29
Emmanuel <emmanuelcd.2211@gmail.com>
2026-08-10 21:59:43 -0600

    jfkdl

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,20 +1,4 @@
 vim.loader.enable()
 
--- Main settings
-require("plugin.00options")
-require("plugin.02autocmds")
-require("plugin.03baseKeymaps")
-require("plugin.04statusLine")
-require("plugin.05theme")
--- PLUGINS (native vim.pack)
--- LSP
-require("plugin.10LSP")
--- Others
-require("plugin.20nvim-treesitter")
-require("plugin.30fzf-lua")
-require("plugin.40mini")
-require("plugin.50luasnip")
-require("plugin.60vim-tmux-navigator")
-require("plugin.80nvim-tree")
-require("plugin.90floatingTerminal")
-require("plugin.99gitsigns")
+-- everything inside nvim/plugin/ will be run automatically at start
+-- so it isn't necessary to require them
