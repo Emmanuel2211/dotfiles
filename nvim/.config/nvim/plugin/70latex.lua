f79cd15a650f082251cc606b0282f59c48fb9909
Emmanuel <emmanuelcd.2211@gmail.com>
2026-08-30 11:15:56 -0600

    latexmk configured with VimTex for compiling with Zathura, Worked!

--- nvim/.config/nvim/plugin/70latex.lua
+++ nvim/.config/nvim/plugin/70latex.lua
@@ -2,7 +2,7 @@
 vim.g.vimtex_imaps_enabled = 0 -- disable imaps (luasnip instead)
 
 -- vimtex view settings... you'll need to configure based on your pdf viewer of choice
-vim.g.vimtex_view_method = "general"
+vim.g.vimtex_view_method = "zathura"
 vim.g.vimtex_view_general_options = "-reuse-instance -forward-search @tex @line @pdf"
 
 vim.pack.add({
